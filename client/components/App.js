c5093ababeaebb9013aa760d284d567d68057d7e
Brian Barrow <briancbarrow@gmail.com>
2016-12-05 06:15:49 -0700

    Lesson 4: React rouer and basic navigation

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Greetings from './greetings';
+import NavigationBar from './NavigationBar';
 
 class App extends React.Component {
   render() {
     return (
-      <Greetings />
+      <div className="conainer">
+        <NavigationBar />
+        {this.props.children}
+      </div>
     );
   }
 }
